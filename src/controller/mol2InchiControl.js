215992be72e5a585957a02b71751f7026028ef89
liudean998 <2510736908@qq.com>
2022-09-13 14:46:19 +0800

    fix: mol2inchi未完成

--- src/controller/mol2InchiControl.js
+++ src/controller/mol2InchiControl.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
+const axios = require("axios")
 const util = require("../utils/util");
 const { v4: uuidv4 } = require("uuid");
 const mol2Inchi = async (ctx) => {
   const { molStr } = ctx.request.query;
   const uuid = uuidv4()
-  fs.writeFile(
-    "C:/Users/Liud/Desktop/projects/server/mol/" + uuid + ".mol",
+  fs.writeFileSync(
+    "mol/" + uuid + ".mol",
     molStr,
-    function (err, res) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("写入成功");
-      }
-    }
+    'utf8'
   );
+  const switchRes = await axios.post('http://43.142.96.10:9001/dpb/execShellScript',{
+    "shellScriptPath": "mol2stdinchi.sh",
+    "inFilePathAndDocName": "/home/centos/mol/HC.mol"
+  })
+  
   ctx.body = { molStr };
 };
 module.exports = {
